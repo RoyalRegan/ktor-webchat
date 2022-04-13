1359b5785c0125609013cde3176156752a92a839
Vlad Anufriev <vanufriev@rtc.usetech.ru>
2022-04-13 17:50:37 +0300

    use hocon config

--- src/main/kotlin/jaju/com/webchat/Application.kt
+++ src/main/kotlin/jaju/com/webchat/Application.kt
@@ -1,15 +1,15 @@
 package jaju.com.webchat
 
-import io.ktor.server.engine.*
+import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import jaju.com.webchat.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureMonitoring()
-        configureSerialization()
-        configureSockets()
-        configureTemplating()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
+    configureMonitoring()
+    configureSerialization()
+    configureSockets()
+    configureTemplating()
 }
